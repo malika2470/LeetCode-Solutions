5c99d640f337a0d00bee5fc3976168fce2b85b3e
malika2470 <55253110+malika2470@users.noreply.github.com>
2024-03-11 12:07:26 -0400

    Time: 71 ms (42.33%), Space: 17.8 MB (29.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_hash = {}
+        n= len(nums)
+
+        for i in range(n): 
+            diff= target-nums[i]
+            if diff in my_hash: 
+                return [my_hash[diff], i]
+            my_hash[nums[i]] =i 
+        return[]
+
+       
+
+        
